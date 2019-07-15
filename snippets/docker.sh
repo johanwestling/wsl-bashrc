6a1fc6768fba57716bcc952baafb9a1e6c457233
Johan Westling <johan@westling.ax>
2019-07-15 17:14:23 +0300

    Minor formatting update

--- snippets/docker.sh
+++ snippets/docker.sh
@@ -1,5 +1,4 @@
 # Autostarts docker service.
-! service docker status | \
-  grep -n 'is running' > /dev/null && \
+! service docker status | grep -n 'is running' > /dev/null && \
   echo -e "\033[33m→\033[39m Starting docker service"; \
   service docker start > /dev/null
