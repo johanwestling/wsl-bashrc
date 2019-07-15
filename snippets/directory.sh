194c0348d209d4c672fac4f9c189bed9b87debc0
Johan Westling <johan@westling.ax>
2019-07-15 19:52:53 +0300

    Added subshell check of directory

--- snippets/directory.sh
+++ snippets/directory.sh
@@ -4,4 +4,6 @@ wsl_root(){
   echo -e "\033[33m→\033[39m Changing to default working directory... (use wsl_root to return at any time)"
   cd /mnt/c/Users/$(cmd.exe /c "echo %USERNAME%" | tr -d '\r')${directory}
 }
-wsl_root
+if [ "$$" -eq "$BASHPID" ]; then 
+  wsl_root
+fi
--- snippets/ip.sh
+++ snippets/ip.sh
@@ -1,5 +1,3 @@
 # Detects current WSL ip address.
-wsl_ip(){
-  echo $(ifconfig eth0 | grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}' | head -1)
-}
-echo -e "\033[32m●\033[39m IP: $(wsl_ip) (use wsl_ip to output ip at any time)"
+export wsl_ip=$(ifconfig eth0 | grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}' | head -1)
+echo -e "\033[32m●\033[39m IP: ${wsl_ip} (use wsl_ip environment variable at any time)"
