cc38ddc2bf88a430b41f968f0105b3b57d7cee20
Johan Westling <johan@westling.ax>
2019-07-16 14:38:50 +0300

    Removed automatic cd command from bashrc

--- snippets/directory.sh
+++ snippets/directory.sh
@@ -4,6 +4,3 @@ wsl_root(){
   echo -e "\033[33m→\033[39m Changing to default working directory... (use wsl_root to return at any time)"
   cd /mnt/c/Users/$(cmd.exe /c "echo %USERNAME%" | tr -d '\r')${directory}
 }
-if [ "$$" -eq "$BASHPID" ]; then 
-  wsl_root
-fi
